e93ac42796655c6b8855718e02c9e19ffb6e5fc5
Sabina <sabina_stoicescu@yahoo.com>
2015-04-01 16:03:03 -0400

    commiting exercise 6 step 3 and 4

--- exercise6.rb
+++ exercise6.rb
@@ -1,6 +1,19 @@
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
-	 grocery_list.each do |x| puts "*#{x}"
-	 end
-	 	grocery_list.push("rice")
-	 	grocery_list.each do |x| puts "*#{x}"
-	end
+	 
+def list(grocery_list)
+	
+grocery_list.each {|x| puts "*#{x}"}
+puts grocery_list.push "rice"
+end
+y="rice"
+list(grocery_list)
+	 #grocery_list.each do |x| puts "*#{x}"
+	 #end
+	 #	grocery_list.push("rice")
+	 #	grocery_list.each do |x| puts "*#{x}"
+	#end
+
+	puts grocery_list.length
+if grocery_list.include? "bananas" then puts "You need to pick up bananas"
+else puts "You don't need to pick up bananas today"
+end
