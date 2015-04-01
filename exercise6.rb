85f285b5ace7bf68f9746176a1d66152d29509a8
Sabina <sabina_stoicescu@yahoo.com>
2015-04-01 15:42:21 -0400

    commiting exercise 6 step 2

--- exercise6.rb
+++ exercise6.rb
@@ -1,3 +1,6 @@
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 	 grocery_list.each do |x| puts "*#{x}"
-end
+	 end
+	 	grocery_list.push("rice")
+	 	grocery_list.each do |x| puts "*#{x}"
+	end
