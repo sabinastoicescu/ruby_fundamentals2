3a8deb8cdeae332d95c206a84de59577503aff05
Sabina <sabina_stoicescu@yahoo.com>
2015-04-02 08:23:02 -0400

    :committing ex 7 point 3

--- exercise7.rb
+++ exercise7.rb
@@ -4,9 +4,10 @@ students = {
 	:cohort3 => 22
 }
 def display_cohorts(students)
-	students.each {|x,y| puts "#{x}: #{y} students"}
+	students.each {|x,y| puts "#{x}"}
 end
 
 
 students[:cohort4] = "43"
 display_cohorts(students)
+
